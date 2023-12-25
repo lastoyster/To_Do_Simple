b6064a8530760e888e967323c47f8e03ecc63c9a
lastoyster <kadampratiksha56@gmail.com>
2023-12-25 19:43:41 +0530

    Added ToDo List  without any  state management lib

---
+++ lib/todo_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'package:todo_list/todo.dart';
+
+import 'main.dart';
+
+typedef ToggleTodoCallback = void function(TodoScreen);
+
+class TodoList extends StatelessWidget {
+  TodoList({@required this.todos, this.onTodoToggle)};
+
+  final List<Todo> todos;
+  final ToggleTodoCallback onTodoToggle;
+
+  Widget _buildItem(BuildContext context, int index){
+    final todo = todos[index];
+
+  return CheckboxListTile( 
+    value:todo.isDone,
+    title:Text(todo.title),
+    onChanged:(bool isChecked){
+      onTodoToggle(todo, isChecked);
+    },
+  );
+  }
+
+@override 
+Widget build(BuildContext context){
+  return ListView.builder( 
+    itemBuilder:_buildItem
+    itemCount:todos.length,
+  );
+}
+}
